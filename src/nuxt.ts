ab404db6a687836482f05fcfe9d771a91c894417
zouhang <zouhang@didiglobal.com>
2021-09-02 20:12:15 +0800

    feat: add exports

---
+++ src/nuxt.ts
@@ -0,0 +1,17 @@
+import unplugin from '.'
+
+export default function(this: any, options: any) {
+  options.exclude = options.exclude || [/node_modules/, /\.git/, /\.nuxt/]
+
+  // install webpack plugin
+  this.extendBuild((config: any) => {
+    config.plugins = config.plugins || []
+    config.plugins.unshift(unplugin.webpack(options))
+  })
+
+  // install vite plugin
+  this.nuxt.hook('vite:extend', async(vite: any) => {
+    vite.config.plugins = vite.config.plugins || []
+    vite.config.plugins.push(unplugin.vite(options))
+  })
+}
